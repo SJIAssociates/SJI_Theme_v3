60685d6bb81c8c0c088fac56a05d94ed9a52491f
Collin B <berg.collin@gmail.com>
2020-06-01 00:57:22 -0400

    Homepage Template

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.app')
 
+@include('partials.header')
+
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+<div class="wrap container" role="document">
+  <div class="content">
+    <main class="main">
+    @while(have_posts()) @php the_post() @endphp
+      @include('partials.page-header')
+      @include('partials.content-page')
+    @endwhile
+    </main>
+    @if (App\display_sidebar())
+      <aside class="sidebar">
+        @include('partials.sidebar')
+      </aside>
+    @endif
+  </div>
+</div>
 @endsection
