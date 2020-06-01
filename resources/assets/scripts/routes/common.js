71bf76f8e80fcc42b7edb963dfc4e59f3b434169
Collin B <berg.collin@gmail.com>
2020-06-01 00:56:28 -0400

    Added Functions to: - Add 3 widgets in footer - Vcard Widget - Remove Comments - Fix titles in Archive - tree function - Removed Protected in title - Cleanup homepage scripts/styles

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,12 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    function menuMorph(){
+			$('.sitewrapper').toggleClass('openNav');
+			$('#menuToggle').toggleClass('open');
+			$('#mega-nav').toggleClass('open');
+		}
+		$('#menuToggle').click(menuMorph);
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
