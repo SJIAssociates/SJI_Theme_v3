ecdf964c559b2cd7f91280b851b5b3f455a874b3
Collin B <berg.collin@gmail.com>
2020-06-01 00:56:45 -0400

    Set up Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,20 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+<footer class="content-info bg-grey-darkest pt-5 lg:pt-20 text-white">
+  <div class="container lg:pb-24">
+    <div class='flex flex-wrap justify-between'>
+      <div class='w-full lg:w-1/2'>
+        @php dynamic_sidebar('footer_left') @endphp
+      </div>
+      <div class='w-full lg:w-1/4'>
+        @php dynamic_sidebar('footer_middle') @endphp
+      </div>
+      <div class='w-full lg:w-1/4'>
+        @php dynamic_sidebar('footer_right') @endphp
+      </div>
+    </div>
+  </div>
+  <div class='container'>
+    <div class='flex flex-row justify-between pb-3'>
+      <span class='copyright'>© {!! date('Y') !!} SJI Associates</span>
+      <span class='wbenc'>Women Owned Enterprise</span>
   </div>
 </footer>
