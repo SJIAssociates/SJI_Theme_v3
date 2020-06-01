becd35902c097c7ed92a5b4b9c062fd54451a4eb
Collin B <berg.collin@gmail.com>
2020-06-01 17:24:19 -0400

    Home placeholder animation.

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -8,7 +8,7 @@
 <div class="flex bg-white min-h-screen">
     <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
         <div>
-            <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">Build Your New <span class="text-indigo-600">Idea</span></h2>
+            <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl animate__animated animate__bounce">Build Your New <span class="text-indigo-600">Idea</span></h2>
             <p class="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
             <div class="flex justify-center lg:justify-start mt-6">
                 <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
