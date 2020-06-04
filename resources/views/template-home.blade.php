7472774ac0363b6039e4db71eff472e4c268bdad
Collin B <berg.collin@gmail.com>
2020-06-04 12:52:53 -0400

    Updated Copy

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -8,8 +8,8 @@
 <div class="flex bg-white min-h-screen">
     <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
         <div>
-            <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl animate__animated animate__bounce">Build Your New <span class="text-indigo-600">Idea</span></h2>
-            <p class="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
+            <h2 class="text-3xl font-semibold md:text-6xl animate__animated animate__bounce">Building Brands</h2>
+            <p class="mt-2 text-sm text-gray-500 md:text-lg">With over 25 years of expertise in branding, communications, style guides, advertising and more, our creative teams are able to execute compelling creative across all categories and industries.</p>
             <div class="flex justify-center lg:justify-start mt-6">
                 <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
                 <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">Learn More</a>
