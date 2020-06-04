9bb092edf96f08ebc526ad57845d3be28a4d110e
Collin B <berg.collin@gmail.com>
2020-06-04 12:53:00 -0400

    Page Type Larger and bolder

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
 <div class="page-header h-48 bg-primary flex flex-col-reverse mb-5 lg:mb-10 pb-4">
   <div class='container'>
-    <h1 class='lg:text-5xl text-white text-center'>{!! App::title() !!}</h1>
+    <h1 class='text-3xl lg:text-6xl text-white text-center font-bold'>{!! App::title() !!}</h1>
   </div>
 </div>
