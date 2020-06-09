5a33a5ffbe89b550ac79acb96831a58e311a4356
Collin B <berg.collin@gmail.com>
2020-06-09 18:05:49 -0400

    Dynamically pull in Typekit

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -33,3 +33,11 @@ library.add(faFacebookF, faTwitter, faInstagram, faYoutube );
 
 // tell FontAwesome to watch the DOM and add the SVGs when it detects icon markup
 dom.watch();
+
+window.WebFontConfig = {  typekit: {id: 'pjh5tdq'}};
+(function() {  var wf = document.createElement('script');
+wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+wf.type = 'text/javascript';  wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})();
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <style>
+  html h1,html h2,html h3{opacity: 0;}
+  html.wf-active h1,html.wf-active h2,html.wf-active h3 {opacity: 1; transition: opacity 0.2s;}
+  </style>
   @php wp_head() @endphp
 </head>
